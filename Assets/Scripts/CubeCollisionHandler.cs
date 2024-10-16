aec015c1949c5e725a4460711a04ce12ae83230c
Dylan M. <120059262+26minatreadt@users.noreply.github.com>
2024-10-16 14:01:45 -0500

    Update CubeCollisionHandler.cs

--- Assets/Scripts/CubeCollisionHandler.cs
+++ Assets/Scripts/CubeCollisionHandler.cs
@@ -9,29 +9,18 @@ public class CubeCollisionHandler : MonoBehaviour
     void Start()
     {
         cubeRenderer = GetComponent<Renderer>();
-        cubeRenderer.material.color = collisionColor; // Set the initial color of the cube
+        cubeRenderer.material.color = collisionColor;
     }
 
-    // This method is called when the collider attached to this GameObject collides with another collider
     void OnCollisionEnter(Collision collision)
     {
-        // Check if the object we collided with has the "Sphere" tag
         if (collision.gameObject.CompareTag("Sphere"))
-        {
-            // Log a message to the console
             Debug.Log("Cube collided with Sphere!");
-            // You can add additional logic here specific to sphere collisions
-        }
     }
-    // This method is called when the trigger collider attached to this GameObject intersects with another collider
+
     void OnTriggerEnter(Collider other)
     {
-        // Check if the object we triggered with has the "Sphere" tag
         if (other.CompareTag("Sphere"))
-        {
-            // Log a message to the console
             Debug.Log("Cube triggered with Sphere!");
-            // You can add additional logic here specific to sphere trigger events
-        }
     }
 }
